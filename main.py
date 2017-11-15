6f73ed15c7e2fbedf6f5324537d67c5389ca70d4
Bojie Jiang <jiangbojie@gmail.com>
2017-11-14 20:36:04 -0800

    Got the list of novel

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
 
+# if __name__ == "__main__":
+#     target = 'http://www.biqukan.com/1_1094/5403177.html'
+#     req = requests.get(url=target)
+#     html = req.text
+#     bf = BeautifulSoup(html, "html.parser")
+#     texts = bf.find_all('div', class_='showtxt')
+#     print(texts[0].text.replace('\xa0' * 8, '\n\n'))
+
+# if __name__ == "__main__":
+#     target = 'http://www.biqukan.com/1_1094/'
+#     req = requests.get(url=target)
+#     html = req.text
+#     div_bf = BeautifulSoup(html, "html.parser")
+#     div = div_bf.find_all('div', class_='listmain')
+#     print(div[0])
+
 if __name__ == "__main__":
-    target = 'http://www.biqukan.com/1_1094/5403177.html'
+    server = 'http://www.biqukan.com'
+    target = 'http://www.biqukan.com/1_1094/'
     req = requests.get(url=target)
     html = req.text
-    bf = BeautifulSoup(html, "html.parser")
-    texts = bf.find_all('div', class_='showtxt')
-    print(texts[0].text.replace('\xa0' * 8, '\n\n'))
+    div_bf = BeautifulSoup(html, "html.parser")
+    div = div_bf.find_all('div', class_='listmain')
+    a_bf = BeautifulSoup(str(div[0]))
+    a = a_bf.find_all('a')
+    for each in a:
+        print(each.string, server + each.get('href'))
